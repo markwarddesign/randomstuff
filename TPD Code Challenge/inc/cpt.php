f1a5fd0277bdfb2e06bcddef8fd2bedde877a4f8
Mark Ward <mark@markwarddesign.com>
2022-07-08 16:55:13 -0600

    Create cpt.php

---
+++ TPD Code Challenge/inc/cpt.php
@@ -0,0 +1,195 @@
+<?php
+// Register Custom Event Type
+function tbd_events() {
+
+	$labels = array(
+		'name'                  => _x( 'Events', 'Event Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Event Type', 'Event Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Events', 'text_domain' ),
+		'name_admin_bar'        => __( 'Event Type', 'text_domain' ),
+		'archives'              => __( 'Event Archives', 'text_domain' ),
+		'attributes'            => __( 'Event Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Event:', 'text_domain' ),
+		'all_items'             => __( 'All Events', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Event', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Event', 'text_domain' ),
+		'edit_item'             => __( 'Edit Event', 'text_domain' ),
+		'update_item'           => __( 'Update Event', 'text_domain' ),
+		'view_item'             => __( 'View Event', 'text_domain' ),
+		'view_items'            => __( 'View Events', 'text_domain' ),
+		'search_items'          => __( 'Search Event', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into event', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this event', 'text_domain' ),
+		'items_list'            => __( 'Events list', 'text_domain' ),
+		'items_list_navigation' => __( 'Events list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter events list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Event Type', 'text_domain' ),
+		'description'           => __( 'Event Type Description', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom-fields', 'page-attributes' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+    'rewrite'      				=> array('slug' => 'event', 'with_front' => false)
+	);
+	register_post_type( 'tpd_events', $args );
+
+}
+add_action( 'init', 'tbd_events', 0 );
+
+
+/**
+ * Retrieving the values:
+ * Custom Event Title = get_post_meta( get_the_ID(), 'tbd_custom-event-title', true )
+ */
+class TBD_events_fields {
+	private $config = '{"title":"Custom Event Fields","description":"Custom Fields for TPD Test.","prefix":"tbd_","domain":"tbd-events","class_name":"TBD_events_fields","post-type":["post"],"context":"normal","priority":"core","cpt":"tpd_events","fields":[{"type":"text","label":"Custom Event Title","id":"tbd_custom-event-title"}]}';
+
+	public function __construct() {
+		$this->config = json_decode( $this->config, true );
+		$this->process_cpts();
+		add_action( 'add_meta_boxes', [ $this, 'add_meta_boxes' ] );
+		add_action( 'admin_head', [ $this, 'admin_head' ] );
+		add_action( 'save_post', [ $this, 'save_post' ] );
+	}
+
+	public function process_cpts() {
+		if ( !empty( $this->config['cpt'] ) ) {
+			if ( empty( $this->config['post-type'] ) ) {
+				$this->config['post-type'] = [];
+			}
+			$parts = explode( ',', $this->config['cpt'] );
+			$parts = array_map( 'trim', $parts );
+			$this->config['post-type'] = array_merge( $this->config['post-type'], $parts );
+		}
+	}
+
+	public function add_meta_boxes() {
+		foreach ( $this->config['post-type'] as $screen ) {
+			add_meta_box(
+				sanitize_title( $this->config['title'] ),
+				$this->config['title'],
+				[ $this, 'add_meta_box_callback' ],
+				$screen,
+				$this->config['context'],
+				$this->config['priority']
+			);
+		}
+	}
+
+	public function admin_head() {
+		global $typenow;
+		if ( in_array( $typenow, $this->config['post-type'] ) ) {
+			?><?php
+		}
+	}
+
+	public function save_post( $post_id ) {
+		foreach ( $this->config['fields'] as $field ) {
+			switch ( $field['type'] ) {
+				default:
+					if ( isset( $_POST[ $field['id'] ] ) ) {
+						$sanitized = sanitize_text_field( $_POST[ $field['id'] ] );
+						update_post_meta( $post_id, $field['id'], $sanitized );
+					}
+			}
+		}
+	}
+
+	public function add_meta_box_callback() {
+		echo '<div class="rwp-description">' . $this->config['description'] . '</div>';
+		$this->fields_table();
+	}
+
+	private function fields_table() {
+		?><table class="form-table" role="presentation">
+			<tbody><?php
+				foreach ( $this->config['fields'] as $field ) {
+					?><tr>
+						<th scope="row"><?php $this->label( $field ); ?></th>
+						<td><?php $this->field( $field ); ?></td>
+					</tr><?php
+				}
+			?></tbody>
+		</table><?php
+	}
+
+	private function label( $field ) {
+		switch ( $field['type'] ) {
+			default:
+				printf(
+					'<label class="" for="%s">%s</label>',
+					$field['id'], $field['label']
+				);
+		}
+	}
+
+	private function field( $field ) {
+		switch ( $field['type'] ) {
+			default:
+				$this->input( $field );
+		}
+	}
+
+	private function input( $field ) {
+		printf(
+			'<input class="regular-text %s" id="%s" name="%s" %s type="%s" value="%s">',
+			isset( $field['class'] ) ? $field['class'] : '',
+			$field['id'], $field['id'],
+			isset( $field['pattern'] ) ? "pattern='{$field['pattern']}'" : '',
+			$field['type'],
+			$this->value( $field )
+		);
+	}
+
+	private function value( $field ) {
+		global $post;
+		if ( metadata_exists( 'post', $post->ID, $field['id'] ) ) {
+			$value = get_post_meta( $post->ID, $field['id'], true );
+		} else if ( isset( $field['default'] ) ) {
+			$value = $field['default'];
+		} else {
+			return '';
+		}
+		return str_replace( '\u0027', "'", $value );
+	}
+
+}
+new TBD_events_fields;
+
+/**
+* Update Archive Title if Custom Field has value
+*/
+function tbd_events_title( $title, $id = null ) {
+ 
+    if ( is_post_type_archive( 'tpd_events' ) ) {
+         // Do stuff
+         $custom_title = get_post_meta( get_the_ID(), 'tbd_custom-event-title', true );
+        // Check if the custom field has a value.
+        if ( ! empty( $custom_title) ) {
+            return $custom_title;
+        }
+    }
+ 
+    return $title;
+}
+add_filter( 'the_title', 'tbd_events_title', 10, 2 );
